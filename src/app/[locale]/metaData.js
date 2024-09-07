c0bbbb6a5d1d0cb403a6eab982137dbdb00c1e4d
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-09-08 01:21:16 +0700

    update META_DATA

--- src/app/[locale]/metaData.js
+++ src/app/[locale]/metaData.js
@@ -1,21 +1,39 @@
 import { APP_URL } from "@/constants/AppConfig";
 
 const META_DATA = {
-  TITLE: "Cong Pham Software Engineer",
+  TITLE: "Cong Pham | Crafting Scalable & Impactful Software Solutions",
   DESCRIPTION:
-    "hi, i'm Cong Pham! i studied computer science at the university of toronto, and i'm currently working at dbt Labs as a Software Engineer on the Metadata team. Prior to that, I've worked at SailPoint, Citigroup, and Citylitics as a Software Developer. I enjoy creating scalable & elegant web/mobile applications that have a real world impact. I'm always learning new technologies, either through hackathons or self-study - and recently became an AWS Certified Developer.",
-  APPLICATION_NAME: "Portfolio",
+    "Welcome to the portfolio of Cong Pham, an accomplished Software Engineer with a rich background in crafting scalable, elegant web and mobile applications. A graduate in Computer Science from the University of Toronto, Cong currently applies his expertise at dbt Labs on the Metadata team. With experience at top-tier companies like SailPoint, Citigroup, and Citylitics, Cong is passionate about driving innovation, solving complex problems, and continuously learning through hackathons and self-study. Cong is also an AWS Certified Developer, committed to delivering real-world impact through technology.",
+  APPLICATION_NAME: "Cong Pham's Portfolio",
   KEYWORDS: [
-    "Cong Pham",
-    "software engineer",
-    "Cong Pham software engineer",
     "Cong Pham portfolio",
-    "Cong Pham projects"
+    "Cong Pham software engineer",
+    "web development",
+    "mobile applications",
+    "AWS certified developer",
+    "dbt Labs engineer",
+    "SailPoint developer",
+    "Citigroup developer",
+    "Citylitics developer",
+    "scalable software solutions"
   ],
   GENERATOR: "NextJS",
   ROBOTS: "index, follow",
-  IMAGE: [`${APP_URL}/profile/background.jpg`],
-  URL: "/"
+  IMAGE: [`${APP_URL}/profile/hero-image.jpg`],
+  URL: "/",
+  AUTHOR: "Cong Pham",
+  OG_TITLE: "Cong Pham | Software Engineer & Creator of Scalable Solutions",
+  OG_DESCRIPTION:
+    "Discover the portfolio and projects of Cong Pham, an expert in developing high-impact, scalable applications with a passion for learning and innovation.",
+  OG_TYPE: "website",
+  OG_IMAGE: [`${APP_URL}/profile/hero-image.jpg`],
+  OG_URL: `${APP_URL}`,
+  TWITTER_CARD: "summary_large_image",
+  TWITTER_TITLE: "Cong Pham | Software Engineer & AWS Certified Developer",
+  TWITTER_DESCRIPTION:
+    "Explore Cong Pham's journey in building elegant and scalable web and mobile applications, alongside his experience at dbt Labs, SailPoint, Citigroup, and more.",
+  TWITTER_IMAGE: [`${APP_URL}/profile/twitter-image.jpg`],
+  TWITTER_CREATOR: "@congpham"
 };
 
 export default META_DATA;

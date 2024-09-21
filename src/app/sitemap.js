7438004e00cabccf423986694ba8374fdd028626
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-09-21 13:55:18 +0700

    congpham sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -7,60 +7,6 @@ export default function sitemap() {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1
-    },
-    {
-      url: `${APP_URL}/en`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/vi`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/zh`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/es`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/de`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/fr`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/ja`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/ko`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
-    },
-    {
-      url: `${APP_URL}/ru`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1
     }
   ];
 }
